11b2a29da64d421de7650d310a7a8d8caddf2d8c
Rui Santos <rsantos@wright-brother.com>
2019-08-08 10:14:06 +0200

    First script

---
+++ automation_installWin.ps1
@@ -0,0 +1,19 @@
+﻿Write-Host '****** Welcome to the Laptoper Installer power by PowerShell and Chocolatry *****'
+
+#Install choco it slef : https://chocolatey.org/docs/installation
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#In case of re-run
+choco upgrade -y chocolatey
+
+#Intall developer programs
+choco install -y notepadplusplus
+choco install -y winscp
+choco install -y putty
+choco install -y qtcreator
+choco install -y vscode
+choco install -y sourcetree
+
+
+
+Write-Host '****** Instalation finished ******'
